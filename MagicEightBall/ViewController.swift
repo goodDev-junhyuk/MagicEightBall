ec4849934b536297fd627eb8dc14c829f9541d7e
junhyuk <dejavukk1@gmail.com>
2021-06-02 23:43:03 +0900

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by junhyuk on 2021/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
