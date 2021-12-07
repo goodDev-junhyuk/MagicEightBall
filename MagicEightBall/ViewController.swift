c6ca8e75b54f3d06ebc3d81958b322300a037f15
junhyuk <dejavukk1@gmail.com>
2021-12-07 17:04:27 +0900

    generate() 메소드 구현으로 코드 간결화.

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no",
         "Absolutely not" ]
     
-    
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var shakeButton: UIButton!
     
@@ -29,14 +28,31 @@ class ViewController: UIViewController {
     @IBAction func shakeButtonTapped(_ sender: UIButton) {
         
         // print("Shake Button..!")
-        
         // answerLabel.text = "button was tapped"
+        // let randomIndex = Int.random(in: 0..<answers.count)
+        // answerLabel.text = answers[randomIndex]
         
-        let randomIndex = Int.random(in: 0..<answers.count)
-        
-        answerLabel.text = answers[randomIndex]
+        generateAnswer()
         
     }
+    
+    // MARK : 기기를 흔들었을때 화면 텍스트가 무작위로 변하게 하는 기능구현
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+
+        // let randomIndex = Int.random(in: 0..<answers.count)
+        // answerLabel.text = answers[randomIndex]
+        
+        generateAnswer()
+    }
+
+    func generateAnswer() {
+        
+        let randomIndex = Int.random(in: 0..<answers.count)
+        answerLabel.text = answers[randomIndex]
+    }
+    
+    
     
 
 
